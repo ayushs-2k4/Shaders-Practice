a9bcaf52081ceae290333036127d3827151e222b
Ayush Singhal <ayushsinghals02@gmail.com>
2024-05-05 11:21:01 +0530

    Initial Commit

---
+++ Shaders Practice/Shaders_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shaders_PracticeApp.swift
+//  Shaders Practice
+//
+//  Created by Ayush Singhal on 05/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Shaders_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
